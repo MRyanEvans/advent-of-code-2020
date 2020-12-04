15dff51164ba015ed6b996394aaf7bf58700fc51
Ryan Evans <evansmatthewryan@gmail.com>
2020-12-04 08:07:31 +0000

    Formatting.

--- day4/day4.sh
+++ day4/day4.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-awk -v RS="" -F'\n' -v OFS=' ' '{$1=$1} 1' input.txt | grep 'byr:.*' |
+set -euo pipefail
+
+awk -v RS="" -F'\n' -v OFS=' ' '{$1=$1} 1' input.txt |
+  grep 'byr:.*' |
   grep 'iyr:.*' |
   grep 'eyr:.*' |
   grep 'hgt:.*' |
@@ -9,7 +12,8 @@ awk -v RS="" -F'\n' -v OFS=' ' '{$1=$1} 1' input.txt | grep 'byr:.*' |
   grep 'pid:.*' |
   wc -l
 
-awk -v RS="" -F'\n' -v OFS=' ' '{$1=$1} 1' input.txt | grep -E 'byr:(19[2-9][0-9]|200[0-2])(\s|$)' |
+awk -v RS="" -F'\n' -v OFS=' ' '{$1=$1} 1' input.txt |
+  grep -E 'byr:(19[2-9][0-9]|200[0-2])(\s|$)' |
   grep -E 'iyr:(201[0-9]|2020)(\s|$)' |
   grep -E 'eyr:(202[0-9]|2030)(\s|$)' |
   grep -E 'hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)(\s|$)' |
