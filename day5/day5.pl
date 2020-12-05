305992d83c35f82d1e8846f5afa1d13cbbb52917
Ryan Evans <evansmatthewryan@gmail.com>
2020-12-05 10:35:27 +0000

    More concise.

--- day5/day5.pl
+++ day5/day5.pl
@@ -3,38 +3,26 @@
 use strict;
 use warnings FATAL => 'all';
 
-my $max = 0;
 my @seats;
-
 open INFILE, 'input.txt';
-my $line;
-while ($line = <INFILE>) {
+while (my $line = <INFILE>) {
     chomp($line);
     my $seat = 0;
     foreach my $char (split //, $line) {
         $seat = $seat << 1;
         if ($char eq 'B' || $char eq 'R') {
-            $seat = $seat + 1;
+            $seat++;
         }
-    }
-    if ($seat > $max) {
-        $max = $seat;
     }
     push @seats, $seat;
 }
 close INFILE;
 
-print "Part 1:  " . $max . "\n";
-
 @seats = sort {$a <=> $b} @seats;
-
-for my $i (0 .. $#seats) {
-    if (!exists($seats[$i + 1])) {
-        last; # we know it's not at the back
-    }
-    my $next = $seats[$i + 1];
+print "Part 1:  " . $seats[-1] . "\n";
+for my $i (0 .. ($#seats - 1)) {
     my $expected_next = $seats[$i] + 1;
-    if ($next != $expected_next) {
+    if ($seats[$i + 1] != $expected_next) {
         print "Part 2:  " . $expected_next . "\n";
     }
 }
