bc2fbda11d59719e2c7ffdeb2401c7e21b981c9c
Ryan Evans <evansmatthewryan@gmail.com>
2020-12-16 11:26:52 +0000

    Tidy up a bit.

--- day16/day16.rb
+++ day16/day16.rb
@@ -6,13 +6,13 @@ my_ticket = []
 nearby_tickets = []
 
 File.open('input.txt', 'r').each_line do |l|
-  next if l.strip == ''
-
-  if l.strip == 'your ticket:'
+  case l.strip
+  when '' then
+    next
+  when 'your ticket:' then
     state = :my_ticket
     next
-  end
-  if l.strip == 'nearby tickets:'
+  when 'nearby tickets:' then
     state = :nearby_tickets
     next
   end
@@ -33,95 +33,56 @@ def value_matches_rule?(value, rule)
 end
 
 def value_matches_any_rule?(value, rules)
-  rules.each do |rule|
-    return true if value_matches_rule?(value, rule)
-  end
-  false
-end
-
-def value_matches_all_rules?(value, rules)
-  rules.each do |rule|
-    return false unless value_matches_rule?(value, rule)
-  end
-  true
-end
-
-def ticket_is_valid?(ticket, rules)
-  ticket.each do |value|
-    return false unless value_matches_any_rule?(value, rules)
-  end
-  true
-end
-
-def rules_matched_by_value(value, rules)
-  matches = []
-  rules.each do |rule|
-    if value_matches_rule?(value, rule)
-      matches << rule
-    end
-  end
-  matches
+  rules.any? { |rule| value_matches_rule?(value, rule) }
 end
 
 def part1(rules, nearby_tickets)
-  error_rate = 0
-  nearby_tickets.each do |ticket|
-    ticket.each do |val|
-      error_rate += val unless value_matches_any_rule?(val, rules)
-    end
+  nearby_tickets.map { |t| t.filter { |val| !value_matches_any_rule?(val, rules) }.reduce(0, &:+) }.reduce(&:+)
+end
+
+def get_candidates_for_ticket(ticket, rules)
+  candidates = {}
+  ticket.each_with_index do |val, i|
+    previous_matches = candidates.fetch(i, [])
+    rules.filter { |rule| value_matches_rule?(val, rule) }.each { |match| previous_matches << match[0] }
+    candidates[i] = previous_matches
   end
-  error_rate
+  candidates
 end
 
 def part2(rules, nearby_tickets, my_ticket)
-  valid_tickets = nearby_tickets.filter { |t| ticket_is_valid?(t, rules) }
-  ticket_candidates = []
-  valid_tickets.each do |ticket|
-    candidates = {}
-    ticket.each_with_index do |val, i|
-      previous_matches = candidates.fetch(i, [])
-      matched_rules = rules_matched_by_value(val, rules)
-      matched_rules.each { |match|
-        previous_matches << match[0]
-      }
-      candidates[i] = previous_matches.uniq
-    end
-    ticket_candidates << candidates
-  end
+  valid_tickets = nearby_tickets.filter { |t| t.all? { |v| value_matches_any_rule?(v, rules) } }
+  ticket_candidates = valid_tickets.map { |ticket| get_candidates_for_ticket(ticket, rules) }
 
   position_candidates = []
   (0..valid_tickets[0].size - 1).each do |pos|
+    # get the candidates for position from each ticket and apply the & operator to find rules common in all positions
     position_candidates << ticket_candidates.map { |t| t[pos] }.inject(&:&)
   end
 
   n = 0
-  while true
+  loop do
     candidates = position_candidates[n]
     if candidates.size == 1
-      position_candidates.each_with_index { |c, i|
-        if i == n
-          next
-        end
-        position_candidates[i] = position_candidates[i].filter { |d| d != candidates[0] }
-      }
+      # narrowed down only one possible candidate for position
+      position_candidates.each_index do |i|
+        next if i == n
+        # remove candidate from other lists as we know it can't go there anymore
+        position_candidates[i] = position_candidates[i].filter { |d| d != candidates.first }
+      end
     end
-    if position_candidates.map(&:size).max == 1
-      break
-    end
-    n += 1
-    if n == position_candidates.size
-      n = 0
-    end
-  end
-  order = position_candidates.flatten
+    # end when everything has exactly one candidate
+    break if position_candidates.map(&:size).max == 1
 
-  ticket_values = {}
-  my_ticket.each_with_index do |v, i|
-    v
-    relative_field = order[i]
-    ticket_values[relative_field] = v
+    n += 1
+    n = 0 if n == position_candidates.size
   end
-  ticket_values.filter { |v| v.start_with?('departure') }.map { |m| m[1] }.reduce(:*)
+
+  my_ticket.each_with_index.map { |v, i| [position_candidates.flatten[i], v] }
+           .to_h
+           .filter { |v| v.start_with?('departure') }
+           .map { |m| m[1] }
+           .reduce(:*)
 end
 
 print "Part 1:  #{part1(rules, nearby_tickets)}\n"
