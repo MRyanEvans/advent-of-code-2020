c24e1aede0b92cda647e17e2d96a168aacae66f1
Ryan Evans <evansmatthewryan@gmail.com>
2020-12-17 18:05:09 +0000

    Tidied up a bit.

--- day17/day17.rb
+++ day17/day17.rb
@@ -6,12 +6,12 @@ z = 0
 w = 0
 File.open('input.txt', 'r').each_line do |l|
   x = 0
-  for c in l.strip.chars
-    if c == '#'
-      cubes[[x, y, z, w]] = :active
-    else
-      cubes[[x, y, z, w]] = :inactive
-    end
+  l.strip.chars.each do |c|
+    cubes[[x, y, z, w]] = if c == '#'
+                            :active
+                          else
+                            :inactive
+                          end
     x += 1
   end
   y += 1
@@ -19,25 +19,25 @@ end
 
 def expand(cubes)
   expanded = {}
-  max_x = cubes.map { |c| c[0][0] }.sort.last
-  max_y = cubes.map { |c| c[0][1] }.sort.last
-  max_z = cubes.map { |c| c[0][2] }.sort.last
-  max_w = cubes.map { |c| c[0][3] }.sort.last
-  for w in (-max_w - 1)..max_w + 1
-    for z in (-max_z - 1)..max_z + 1
-      for y in 0..max_y + 1
-        for x in 0..max_x + 1
+  max_x = cubes.map { |c| c[0][0] }.max
+  max_y = cubes.map { |c| c[0][1] }.max
+  max_z = cubes.map { |c| c[0][2] }.max
+  max_w = cubes.map { |c| c[0][3] }.max
+  ((-max_w - 1)..max_w + 1).each do |w|
+    ((-max_z - 1)..max_z + 1).each do |z|
+      (0..max_y + 1).each do |y|
+        (0..max_x + 1).each do |x|
           if x == max_x + 1 || y == max_y + 1
             expanded[[x + 1, y + 1, z, w]] = :inactive
-          elsif x == 0 || y == 0
+          elsif x.zero? || y.zero?
             expanded[[x, y, z, w]] = :inactive
             expanded[[x + 1, y + 1, z, w]] = cubes[[x, y, z, w]]
           else
-            if cubes[[x, y, z, w]] == nil
-              expanded[[x + 1, y + 1, z, w]] = :inactive
-            else
-              expanded[[x + 1, y + 1, z, w]] = cubes[[x, y, z, w]]
-            end
+            expanded[[x + 1, y + 1, z, w]] = if cubes[[x, y, z, w]] == nil
+                                               :inactive
+                                             else
+                                               cubes[[x, y, z, w]]
+                                             end
           end
         end
       end
@@ -48,23 +48,21 @@ end
 
 def cycle(cubes)
   new_layer = {}
-  for cube in cubes
+  cubes.each do |cube|
     x, y, z, w = cube[0]
     neighbours = get_neighbour_coordinates(x, y, z, w)
     active_neighbours = neighbours.map { |n| cubes[[n[0], n[1], n[2], n[3]]] }.filter { |v| v == :active }
-    if cube[1] == :active
-      if active_neighbours.count == 2 || active_neighbours.count == 3
-        new_layer[[x, y, z, w]] = :active
-      else
-        new_layer[[x, y, z, w]] = :inactive
-      end
-    else
-      if active_neighbours.count == 3
-        new_layer[[x, y, z, w]] = :active
-      else
-        new_layer[[x, y, z, w]] = :inactive
-      end
-    end
+    new_layer[[x, y, z, w]] = if cube[1] == :active
+                                if active_neighbours.count == 2 || active_neighbours.count == 3
+                                  :active
+                                else
+                                  :inactive
+                                end
+                              elsif active_neighbours.count == 3
+                                :active
+                              else
+                                :inactive
+                              end
   end
   new_layer
 end
@@ -74,18 +72,16 @@ def print_layer(cubes, z)
   max_y = cubes.filter { |c| c[2] == z }.map { |c| c[0][1] }.sort.last
   buffer = ''
   buffer += "z=#{z}\n"
-  for y in 0..max_y
-    for x in 0..max_x
+  (0..max_y).each do |y|
+    (0..max_x).each do |x|
       c = cubes[[x, y, z]]
-      if c == :active
-        char = '#'
-      else
-        char = '.'
-      end
+      char = if c == :active
+               '#'
+             else
+               '.'
+             end
       buffer << char
-      if x == max_x
-        buffer += "\n"
-      end
+      buffer += "\n" if x == max_x
     end
   end
   print buffer += "\n"
@@ -97,7 +93,7 @@ def get_neighbour_coordinates(x, y, z, w)
   $permut_cache.map { |c| [c[0] + x, c[1] + y, c[2] + z, c[3] + w] }.filter { |c| c != [x, y, z, w] }
 end
 
-for n in 0..5
+(0..5).each do |n|
   expanded = expand(cubes)
   cycled_cubes = cycle(expanded)
   count = cycled_cubes.count
